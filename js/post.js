15ac271921b4aa32b8312c2b97e5ca3123e025ed
Angelica M.S <90898648+totaldekadens@users.noreply.github.com>
2022-03-28 21:59:22 +0200

    Add files via upload

---
+++ js/post.js
@@ -0,0 +1,27 @@
+
+let burger = document.querySelector(".burger")
+let home = document.querySelector(".home2")
+let about = document.querySelector(".about2")
+let blog = document.querySelector(".blog2")
+
+burger.addEventListener("click", () => {
+    let menu = document.querySelector(".menu")
+    menu.classList.toggle("none");      
+})
+
+home.addEventListener("click", () => {
+
+    console.log("Tjena")
+    let menu = document.querySelector(".menu")
+    menu.classList.add("none");
+})
+
+about.addEventListener("click", () => {
+    let menu = document.querySelector(".menu")
+    menu.classList.add("none");
+})
+
+blog.addEventListener("click", () => {
+    let menu = document.querySelector(".menu")
+    menu.classList.add("none");
+})
